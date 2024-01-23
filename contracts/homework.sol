a2af1c561c2849cc1e408c9f09b71879c8c71c9f
kiên phạm <fbkienpt@gmail.com>
2024-01-23 15:06:41 +0700

    add: homework week 2 about solidity

---
+++ contracts/homework.sol
@@ -0,0 +1,35 @@
+// SPDX-License-Identifier: MIT
+// Code by KienPT :))
+pragma solidity ^0.8.3;
+
+contract HomeWork {
+    constructor() {}
+
+    //Ex1:
+    bool public b = true;
+    
+    function get_b() public view returns(bool) {
+        return b;
+    }
+
+    //Ex2:
+    uint public x = 0;
+    function addToX2(uint y) public {
+        x = y;
+    }
+
+    //Ex3:
+    modifier solve1(uint _x) {
+        require(_x>0, "x khong tang");
+        x+=_x;
+        _;
+    }
+
+    function increaseX(uint _x) public solve1(_x){}
+
+    //Ex4:
+    function returnTwo() public pure returns(int value, bool _value) {
+        value = -2;
+        _value = true;
+    }
+}
